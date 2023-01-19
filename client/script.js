7797c8a6ac69a5832e992f67aa03ed65a1106e58
Allan Adimoolah <allanstarr023@yahoo.com>
2023-01-19 15:41:28 -0400

    update the server url back to localhost

--- client/script.js
+++ client/script.js
@@ -95,7 +95,7 @@ const handleSubmit = async (e) => {
     typeText(messageDiv, previousResponse);
   } else {
     //fetch data from server -> bot's response
-    const response = await fetch('https://starr-codex.onrender.com', {
+    const response = await fetch('http://localhost:5000', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
